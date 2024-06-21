a472000e017c7b0e35c8d99f8ffd70981a640d7c
Mustafa Elbehery <melbeher@redhat.com>
2024-06-21 23:27:39 +0200

    add log sync

--- oo/logger/logger.go
+++ oo/logger/logger.go
@@ -1,77 +1,89 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
-    "time"
+	"fmt"
+	"io"
+	"os"
+	"time"
 )
-
 
 // Level is a log level.
 type Level byte
 
 const (
-    Debug Level = iota + 1
-    Info
-    Error
+	Debug Level = iota + 1
+	Info
+	Error
 )
 
 func (l Level) String() string {
-    switch l {
-    case Debug:
-        return "DEBUG"
-    case Info:
-        return "INFO"
-    case Error:
-        return "ERROR"
-    }
+	switch l {
+	case Debug:
+		return "DEBUG"
+	case Info:
+		return "INFO"
+	case Error:
+		return "ERROR"
+	}
 
-    return fmt.Sprintf("Level <%d>", l)
+	return fmt.Sprintf("Level <%d>", l)
+}
+
+type syncer interface {
+	Sync() error
+}
+
+type nopSyncer struct{}
+
+func (n nopSyncer) Sync() error {
+	return nil
 }
 
 type Logger struct {
-    level Level
-    w     io.Writer
+	level Level
+	w     io.Writer
+	s     syncer
 }
 
 func NewLogger(level Level, out io.Writer) Logger {
-    l := Logger{level, out}
-    return l
+	l := Logger{level, out, nopSyncer{}}
+	if s, ok := out.(syncer); ok {
+		l.s = s
+	}
+	return l
 }
-
 
 func (l Logger) log(level Level, format string, args ...any) {
-    if level < l.level {
-        return
-    }
+	if level < l.level {
+		return
+	}
 
-    msg := fmt.Sprintf(format, args...)
-    ts := time.Now().UTC().Format(time.RFC3339)
-    fmt.Fprintf(l.w, "[%s] - %s - %s\n", ts, level, msg)
+	msg := fmt.Sprintf(format, args...)
+	ts := time.Now().UTC().Format(time.RFC3339)
+	fmt.Fprintf(l.w, "[%s] - %s - %s\n", ts, level, msg)
+
+	l.s.Sync()
+	g
 }
-
-
 
 // Debug is a debug log.
 func (l Logger) Debug(format string, args ...any) {
-    l.log(Debug, format, args...)
+	l.log(Debug, format, args...)
 }
 
 // Info is an info log.
 func (l Logger) Info(format string, args ...any) {
-    l.log(Info, format, args...)
+	l.log(Info, format, args...)
 }
 
 // Error is an error log.
 func (l Logger) Error(format string, args ...any) {
-    l.log(Error, format, args...)
+	l.log(Error, format, args...)
 }
 
-
 func main() {
-    log := NewLogger(Info, os.Stdout)
-    log.Debug("debug")
-    log.Info("info")
-    log.Error("error")
+	log := NewLogger(Info, os.Stdout)
+	log.Debug("debug")
+	log.Info("info")
+	log.Error("error")
 }
