d5f39c5ebfab87518157feca6fd0f540d4fb2222
Mustafa Elbehery <melbeher@redhat.com>
2024-06-21 18:35:10 +0200

    add server port option

--- myfunc/myserver/myserver.go
+++ myfunc/myserver/myserver.go
@@ -1,4 +1,9 @@
-package myserver
+package main
+
+import (
+	"fmt"
+	"log"
+)
 
 type Server struct {
 	verbose bool
@@ -21,4 +26,26 @@ func NewServer(options ...func(*Server) error) (*Server, error) {
 func WithVerbose(srv *Server) error {
 	srv.verbose = true
 	return nil
+}
+
+const portErrFmt = "port must be between 0 and %d, got %d"
+
+func WithPort(port int) func(*Server) error {
+	const maxPort = 0xFFFF
+	return func(server *Server) error {
+		if port <= 0 || port > maxPort {
+			return fmt.Errorf(portErrFmt, maxPort, port)
+		}
+		server.port = port
+		return nil
+	}
+}
+
+func main() {
+	srv, err := NewServer(WithPort(9999), WithVerbose)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	fmt.Printf("%#v\n", srv)
 }
